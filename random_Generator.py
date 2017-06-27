dd8e6416e945bdac99dea8f5c114e96b88ba217d
namnamnam <namkwanwoo94@gmail.com>
2017-06-27 19:10:38 +0900

    Fin) making online users data (= Consumer + Seller)

--- random_Generator.py
+++ random_Generator.py
@@ -1,6 +1,9 @@
 def baseN(num,b,numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
     return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
 
-for internalID in range(1,200):
+print ("ID, personal_Phone, e-mail")
+
+for internalID in range(1,231):
     mangled = (internalID*1679979167)%(36**6)
-    print (internalID, mangled, baseN(mangled,36))
+    #print (internalID, mangled + (internalID*167997)%(36**3), baseN(mangled,36) + '@' + baseN(mangled,36) + '.com')
+    print(baseN(mangled,36) + ' GU ' + baseN(mangled,25) + ' Dong')
--- script.py
+++ script.py
@@ -1,18 +1,24 @@
 f = open("/Users/namnamnam/Desktop/person_List.txt", 'w')
+
+id_list = open("/Users/namnamnam/Desktop/ID.txt", 'r')
 name_list = open("/Users/namnamnam/Desktop/listOfName.txt", 'r')
-
-
+pw_list = open("/Users/namnamnam/Desktop/password_List.txt", 'r')
+email_list = open("/Users/namnamnam/Desktop/email.txt", 'r')
+ph_list = open("/Users/namnamnam/Desktop/phoneNumber.txt", 'r')
+addr_list = open("/Users/namnamnam/Desktop/Address.txt", 'r')
 
 while True:
-    line = name_list.readline()
-    if not line: break
+    id_line = id_list.readline()
+    name_line = name_list.readline()
+    pw_line = pw_list.readline()
+    email_line = email_list.readline()
+    ph_line = ph_list.readline()
+    addr_line = addr_list.readline()
+
+    if not id_line: break
     data = "INSERT INTO Person (NamDB.Person.UserID, NamDB.Person.Password, NamDB.Person.Name, NamDB.Person.Email, NamDB.Person.Phone, NamDB.Person.Address)\n"
     f.write(data)
-    data = "VALUES ('%s', 'skarhksdn', 'NamKwanWoo', 'namkwanwoo94@gmail.com', '01063649614', 'Seoul Seonbukgu');\n" % line
+    data = "VALUES ('%s', '%s', '%s', '%s', '%s', '%s');\n\n" % (id_line , pw_line , name_line ,email_line , ph_line , addr_line)
     f.write(data)
-    print(line)
 
 f.close()
-
-
-
